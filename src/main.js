9346ed248cfe30b0a6f36c41345301aeb06691d6
Yoshiki Takeoka <yoshikinoko@gmail.com>
2019-03-20 17:25:04 +0900

    Distribute splited texts to vertical (newlines), and horizontal (tabs, commas).

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ async function showAlert() {
   return alert('SplitText', 'Please select text element(s).');
 }
 
-async function split(separator) {
+async function split(param) {
   const texts = selection.items.filter(element => element instanceof Text);
 
   if (texts.length === 0) {
@@ -18,7 +18,7 @@ async function split(separator) {
     return 1;
   }
   texts.forEach(text => {
-    const lines = text.text.split(separator);
+    const lines = text.text.split(param.separator);
     const lineGroup = [];
     lines.forEach(line => {
       if (line !== '') {
@@ -34,23 +34,35 @@ async function split(separator) {
     if (lineGroup.length > 0) {
       const lastLine = lineGroup[lineGroup.length - 1];
       selection.items = [text, lastLine];
-      commands.alignBottom();
-      selection.items = lineGroup;
-      commands.distributeVertical();
+      switch (param.distribution) {
+        case 'vertical':
+          commands.alignBottom();
+          selection.items = lineGroup;
+          commands.distributeVertical();
+          break;
+        case 'horizontal':
+          commands.alignRight();
+          selection.items = lineGroup;
+          commands.distributeHorizontal();
+          break;
+        default:
+          break;
+      }
       commands.group();
+      selection.items[0].name = text.text;
     }
   });
   return 0;
 }
 
 async function splitbynewline() {
-  await split('\n');
+  await split({ separator: '\n', distribution: 'vertical' });
 }
 async function splitbytab() {
-  await split('\t');
+  await split({ separator: '\t', distribution: 'horizontal' });
 }
 async function splitbycomma() {
-  await split(',');
+  await split({ separator: ',', distribution: 'horizontal' });
 }
 
 module.exports = {
